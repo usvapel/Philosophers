5ea59833f43d83c4635d488065dfa5c9e898a8b5
Julius Pellinen <jpelline@c1r5p5.hive.fi>
2025-06-18 20:55:10 +0300

    philo

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -13,6 +13,9 @@
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <pthread.h>
 # include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,14 +6,42 @@
 /*   By: jpelline <jpelline@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 20:54:16 by jpelline          #+#    #+#             */
-/*   Updated: 2025/06/16 20:54:25 by jpelline         ###   ########.fr       */
+/*   Updated: 2025/06/17 19:57:15 by jpelline         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main()
+static void	print_help(void)
 {
-	printf("hello world");
-	return 0;
+	const char	*help_text;
+
+	help_text
+		="┌─────────────────────────────────────────────────────────────────┐\n"
+		"│                            Philosophers                         │\n"
+		"├─────────────────────────────────────────────────────────────────┤\n"
+		"│ Usage: ./philo <philosophers> <die> <eat> <sleep> [times_to_eat]│\n"
+		"│                                                                 │\n"
+		"│ Arguments:                                                      │\n"
+		"│   philosophers - Number of philosophers (and forks)             │\n"
+		"│   die          - Time (ms) before death without eating          │\n"
+		"│   eat          - Time (ms) to eat                               │\n"
+		"│   sleep        - Time (ms) to sleep                             │\n"
+		"│   times_to_eat - [Optional] Times each philosopher must eat     │\n"
+		"│                                                                 │\n"
+		"│ Example: ./philo 5 800 200 200 3                                │\n"
+		"└─────────────────────────────────────────────────────────────────┘\n";
+	printf("%s", help_text);
+}
+
+int	main(int ac, char **av)
+{
+	(void)av;
+	if (ac == 5 || ac == 6)
+	{
+		printf("test");
+	}
+	else
+		print_help();
+	return (0);
 }
